eeea3348ffa5daadbae61791b8998b2e27b740a5
Mehdi <mohammadmoozeh@gmail.com>
2020-04-03 18:24:53 +0430

    feat: socket app

---
+++ episode4_Deploy_Our_Apps/socketApp/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'index.html'))
+});
+
+io.on('connection', socket => {
+  console.log('User connected :)');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('pingServer', () => {
+    socket.emit('pingAnswer', String(new Date() + `, version: ${process.env.version}`))
+  })
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`LISTEN ON PORT ${PORT}`);
+});
+
